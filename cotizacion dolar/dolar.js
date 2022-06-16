034a97af400a030866a1d24043ad0c7093cd3b95
mfolino <matefolino@gmail.com>
2022-06-16 19:22:13 -0300

    Dolar

--- cotizacion dolar/dolar.js
+++ cotizacion dolar/dolar.js
@@ -1,17 +1,22 @@
 function cargarDolar() {
 
 
-    fetch('datosdol.json')
-        .then(cotizacion => cotizacion.json())
-        //.then(cotizacion => console.log(cotizacion))
+    // fetch('datosdol.json')
+    //     .then(cotizacion => cotizacion.json())
+    //     //.then(cotizacion => console.log(cotizacion))
 
 
-        .then(cotizaciones => {
-            cotizaciones.forEach(cotiza => {
-                console.log(cotiza);
-            });
+    //     .then(cotizaciones => {
+    //         cotizaciones.forEach(cotiza => {
+    //             console.log(cotiza);
+    //         });
 
-        })
-
+    //     })
+    fetch('https://www.dolarsi.com/api/api.php?type=valoresprincipales')
+    // Exito
+    .then(response => response.json())  // convertir a json
+    .then(json => console.log(json))    //imprimir los datos en la consola
+    .catch(err => console.log('Solicitud fallida', err)); // Capturar errores
+    
 }
 cargarDolar();
