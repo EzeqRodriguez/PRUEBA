cf863dfa993666e0ab60d384bbeb7e0046f30105
mfolino <matefolino@gmail.com>
2022-07-17 23:27:55 -0300

    pyHB

--- sprint5.py
+++ sprint5.py
@@ -1,4 +1,4 @@
-f = open('./PRUEBA/HomeBanking/cheques.html', 'w')
+f = open('PRUEBA/HomeBanking/cheques.html', 'w')
 
 html_template = """<html>
 <head>
@@ -63,9 +63,6 @@ html_template = """<html>
             </div>
           </nav>
 </header>
-<script>
-
-</script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 </body>
